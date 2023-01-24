2fe58713e3e131662070e61d8c2968aced687404
Jegor <jegor@jegor.ee>
2023-01-24 18:16:29 +0200

    Moved to saga: userauth, user session, user login

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,42 +1,41 @@
-import { Fragment } from 'react';
-import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Fragment } from "react";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import CartIcon from '../../components/cart-icon/cart-icon.component';
-import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
-import { selectIsCartOpen } from '../../store/cart/cart.selector';
-import { selectCurrentUser } from '../../store/user/user.selector';
+import { selectIsCartOpen } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
-import { ReactComponent as StoreLogo } from '../../assets/logo.svg';
-import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { ReactComponent as StoreLogo } from "../../assets/logo.svg";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import {
   NavigationContainer,
   NavLinks,
   NavLink,
   LogoContainer,
-} from './navigation.styles.jsx';
+} from "./navigation.styles.jsx";
 
 const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
-  console.log(isCartOpen);
 
   return (
     <Fragment>
       <NavigationContainer>
-        <LogoContainer to='/'>
+        <LogoContainer to="/">
           <StoreLogo />
         </LogoContainer>
         <NavLinks>
-          <NavLink to='/shop'>SHOP</NavLink>
+          <NavLink to="/shop">SHOP</NavLink>
           {currentUser ? (
-            <NavLink as='span' onClick={signOutUser}>
+            <NavLink as="span" onClick={signOutUser}>
               LOG OUT
             </NavLink>
           ) : (
-            <NavLink to='/auth'>LOGIN</NavLink>
+            <NavLink to="/auth">LOGIN</NavLink>
           )}
           <CartIcon />
         </NavLinks>
