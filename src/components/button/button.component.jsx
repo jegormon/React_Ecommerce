14c518eee49e81b3c6f6e8109bcb379bdda727ea
Jegor <jegor@jegor.ee>
2023-01-27 12:29:52 +0200

    Integrated Stripe payment gateway

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -2,12 +2,13 @@ import {
   BaseButton,
   GoogleSignInButton,
   InvertedButton,
-} from './button.styles.jsx';
+  ButtonSpinner,
+} from "./button.styles.jsx";
 
 export const BUTTON_TYPE_CLASSES = {
-  base: 'base',
-  google: 'google-sign-in',
-  inverted: 'inverted',
+  base: "base",
+  google: "google-sign-in",
+  inverted: "inverted",
 };
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
@@ -17,9 +18,13 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, ...inputOptions }) => {
+const Button = ({ children, buttonType, isLoading, ...inputOptions }) => {
   const CustomButton = getButton(buttonType);
-  return <CustomButton {...inputOptions}>{children}</CustomButton>;
+  return (
+    <CustomButton disabled={isLoading} {...inputOptions}>
+      {isLoading ? <ButtonSpinner /> : children}
+    </CustomButton>
+  );
 };
 
 export default Button;
